e6950a29b859f34a8f00eb5d42a0158c318f6685
armikhalev <armikhalev@gmail.com>
2018-03-24 23:49:01 -0400

    Adds id done and title hash-map

--- src/cljs/todo_app/pages/home_page.cljs
+++ src/cljs/todo_app/pages/home_page.cljs
@@ -1,16 +1,24 @@
 (ns todo-app.pages.home-page
   (:require [reagent.core :as r]))
 
-(defonce app-state  (r/atom {:tasks ["one", "two"]}))
+(defonce counter (r/atom 0))
 
-;; -------------------------
-;; Views
+(defonce app-state (r/atom {:tasks (hash-map)}))
+
+(defn delete-task [id title]
+  [:button
+   {:style {:margin 10
+            :padding 10
+            :fontWeight "bold"}
+    }
+   "Delete me"])
 
 (defn task-list-comp [tasks]
   [:ul
-   (for [[index task] (map-indexed vector @tasks)]
-     ^{:key index}
-     [:li task])])
+   (for [[id {done :done title :title}] @tasks]
+     ^{:key id}
+     [:li title
+      [delete-task id title]])])
 
 (defn task-input-comp [tasks]
   (let [task-input (r/atom "")]
@@ -25,13 +33,13 @@
          :on-key-up
          (fn [e]
            (when (= "Enter" (.-key e))
-             (swap! tasks #(conj % @task-input))
+             (let [id (swap! counter inc)]
+               (swap! tasks assoc id {:title @task-input :done false}))
              (reset! task-input "")
              (.preventDefault e)))} ]])))
 
 (defn home-page []
-  [:div {:style {:color "red"
-                 :backgroundColor "yellow"}}
+  [:div 
    [:h2 "Welcometh to todo-app"]
    [:div
     [task-input-comp (r/cursor app-state [:tasks])]
